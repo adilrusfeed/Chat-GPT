c56dc47345cc5fef18993e659e2ea4f153ba75fb
adilrusfeed <adilrusfeed@gmail.com>
2024-08-01 15:44:47 +0530

    API initialize

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+// String API_KEY = "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";
