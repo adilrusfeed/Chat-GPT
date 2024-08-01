1a771ac8f6823a1ad9e319fb425144d717304ae1
adilrusfeed <adilrusfeed@gmail.com>
2024-08-01 15:19:05 +0530

    API initialize

---
+++ lib/constants/api_constant.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "sk-None-1uV3PEeLZojNm1xQS7XqT3BlbkFJMhCTF5zAQJlUwDlEIz0D";
---
+++ lib/service/service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:chatgpt/constants/api_constant.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static Future<void> getModels() async {
+    try {
+      var response = await http.get(Uri.parse("$BASE_URL/models"),
+          headers: {'Authorization': 'Bearer $API_KEY'});
+      Map jsonResponse = jsonDecode(response.body);
+      print("jsonResponse $jsonResponse");
+    } catch (error) {
+      print("error $error");
+    }
+  }
+}
