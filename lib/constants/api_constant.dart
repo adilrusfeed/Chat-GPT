2ba4438e294d493b50b107a4fd73affc99a0bb31
adilrusfeed <adilrusfeed@gmail.com>
2024-08-04 00:16:32 +0530

    API initializing

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -1,1 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "";
--- lib/service/service.dart
+++ lib/service/service.dart
@@ -8,7 +8,7 @@ class ApiService {
   static Future<void> getModels() async {
     try {
       var response = await http.get(Uri.parse("$BASE_URL/models"),
-          headers: {'Authorization': 'Bearer '});
+          headers: {'Authorization': 'Bearer $API_KEY'});
       Map jsonResponse = jsonDecode(response.body);
       if (jsonResponse['error'] != null) {
         print("jsonResponse['error] ${jsonResponse['error']['message']}");
